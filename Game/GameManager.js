73e71e39a5e2d94528b14a3f39c1bb335073f6e9
LanaBesevic <lana.besevic@gmail.com>
2017-11-24 09:19:41 +0100

    Added floor

--- Game/GameManager.js
+++ Game/GameManager.js
@@ -14,22 +14,31 @@ class GameManager {
             self.camera.aspect = canvas.clientWidth / canvas.clientHeight;
         });
 
+
         this.scene = new Scene();
+
         this.player = new Player(this);
         this.scene.add(this.player);
-        this.bird = new MeshObject();
-        this.player.add(this.bird);
+
+        //this.bird = new MeshObject();
+        //this.player.add(this.bird);
+
+        this.floor = new MeshObject();
+        this.scene.add(this.floor);
+        this.floor.scale = new GLMath.Vector3(100, 100, 100);
+        this.floor.translateY(-4);
 
         this.scene.add(new DirectionalLight("rgb(255, 255, 255)", 0.6, new GLMath.Vector3(0, 0, 1)));
         this.plightA = new PointLight("rgb(255, 255, 255)", 0.6, 0, 250);
         this.scene.add(this.plightA);
-
         this.plightB = new PointLight("rgb(255, 255, 255)", 0.6, 0, 250);
         this.scene.add(this.plightB);
+        this.scene.add(new AmbientLight("rgb(50, 50, 50)"));
 
         this.camera = new Camera();
-        this.scene.add(new AmbientLight("rgb(50, 50, 50)"));
-        this.camera.translateZ(20);
+        this.camera.translateZ(40);
+        this.camera.translateY(30);
+        this.camera.rotateX(-Math.PI/5);
 
         this.elapsed_time = 0;
 
@@ -40,15 +49,20 @@ class GameManager {
         let self = this;
         OBJ.downloadMeshes({
             'player': 'Models/LowPolyMan.obj', // located in the models folder on the server
-            'bird': 'Models/Bird.obj'
+            //'bird': 'Models/Bird.obj',
+            'floor': 'Models/floorbig.obj'
         }, function(meshes) {
             self.player.initModelData(meshes.player);
             self.player.material.texture = new Image();
             self.player.material.texture.src = 'Textures/LowPolyMan5eyes.png';
 
-            self.bird.initModelData(meshes.bird);
-            self.bird.material.texture = new Image();
-            self.bird.material.texture.src = 'Textures/bird.png';
+            //self.bird.initModelData(meshes.bird);
+            //self.bird.material.texture = new Image();
+            //self.bird.material.texture.src = 'Textures/bird.png';
+
+            self.floor.initModelData(meshes.floor);
+            self.floor.material.texture = new Image();
+            self.floor.material.texture.src = 'Textures/floor64x.png';
         });
     }
 
--- Renderer/TextureManager.js
+++ Renderer/TextureManager.js
@@ -9,6 +9,8 @@ class TextureManager {
         this._gl.bindTexture(this._gl.TEXTURE_2D, texture);
         this._gl.pixelStorei(this._gl.UNPACK_FLIP_Y_WEBGL, true);
         this._gl.texImage2D(this._gl.TEXTURE_2D, 0, this._gl.RGBA, this._gl.RGBA, this._gl.UNSIGNED_BYTE, img);
+        this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_WRAP_S, this._gl.REPEAT);
+        this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_WRAP_T, this._gl.REPEAT);
         this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_MAG_FILTER, this._gl.LINEAR);
         this._gl.texParameteri(this._gl.TEXTURE_2D, this._gl.TEXTURE_MIN_FILTER, this._gl.LINEAR_MIPMAP_NEAREST);
         this._gl.generateMipmap(this._gl.TEXTURE_2D);
