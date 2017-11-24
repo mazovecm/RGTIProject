e50e63d7270e00cd603fe8994422fb7b65de2cce
mazovecm <33516089+mazovecm@users.noreply.github.com>
2017-11-24 11:47:57 +0100

    histrost osebka

--- Game/Player.js
+++ Game/Player.js
@@ -9,16 +9,28 @@ class Player extends MeshObject {
     update(dt) {
         let input_set = this.input_manager.getPressedKeys();
 
-        if (input_set.has(83)) {
+        if (input_set.has(83) && input_set.has(16)) {
+            this.translateX(dt * -0.01);
+        } else if (input_set.has(83)) {
             this.translateX(dt * -0.005);
         }
-        if (input_set.has(87)) {
+
+
+        if (input_set.has(87) && input_set.has(16)) {
+            this.translateX(dt * 0.01);
+        } else if (input_set.has(87)) {
             this.translateX(dt * 0.005);
         }
-        if (input_set.has(65)) {
+
+        if (input_set.has(65) && input_set.has(16)) {
+            this.rotateY(dt * 0.01);
+        } else if (input_set.has(65)) {
             this.rotateY(dt * 0.005);
         }
-        if (input_set.has(68)) {
+
+        if (input_set.has(68) && input_set.has(16)) {
+            this.rotateY(dt * -0.01);
+        } else if (input_set.has(68)) {
             this.rotateY(dt * -0.005);
         }
     }
